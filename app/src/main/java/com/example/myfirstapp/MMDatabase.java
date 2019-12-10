0155eb009782556dd3e0a32f986a4e963f4db34d
Rowin <rowin@coable.nl>
2019-12-10 17:47:19 +0100

    Write data to external storage as backup

--- app/src/main/java/com/example/myfirstapp/MMDatabase.java
+++ app/src/main/java/com/example/myfirstapp/MMDatabase.java
@@ -2,6 +2,7 @@ package com.example.myfirstapp;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.util.Log;
 
 import androidx.room.Database;
@@ -10,10 +11,14 @@ import androidx.room.RoomDatabase;
 
 import com.google.gson.Gson;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,14 +34,17 @@ public abstract class MMDatabase extends RoomDatabase {
     // The only instance
     private static MMDatabase Database;
 
+    private String filename = "BierGebruik.txt";
+    private String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MM/DATA/";
+
     public static synchronized MMDatabase getInstance(Context context) {
         if (Database == null) {
             // There is no database yet
             Database = Room.databaseBuilder(context.getApplicationContext(), MMDatabase.class, "db")
                     .allowMainThreadQueries()
                     .build();
-                    // .allowMainThreadQueries() is a dirty way to be able to get data from my db.
-                    // TODO: switch from allowMainThreadQueries to rxJava. even if its an assload of work with weird syntax
+            // .allowMainThreadQueries() is a dirty way to be able to get data from my db.
+            // TODO: switch from allowMainThreadQueries to rxJava. even if its an assload of work with weird syntax
 
             Database.populateDatabaseRandomData();
             Database.printDB();
@@ -45,66 +53,67 @@ public abstract class MMDatabase extends RoomDatabase {
     }
 
     public abstract BewonerDAO getBewonerDAO();
+
     public abstract SchoonmaakBierDAO getSchoonmaakBierDAO();
 
     protected synchronized void populateDatabase() {
 
-                    // Dirty way to check if this database already has data. I could not find a good way to do this
-                    List<Bewoner> listBewoners = getBewonerDAO().getAllBewoners();
-                    if(!listBewoners.isEmpty()) {
-                        return;
-                    }
+        // Dirty way to check if this database already has data. I could not find a good way to do this
+        List<Bewoner> listBewoners = getBewonerDAO().getAllBewoners();
+        if (!listBewoners.isEmpty()) {
+            return;
+        }
 
 
-                    Log.d("IK BENEEN NIEUWE THREAD", "Ik Ga nu de database vullen met start data");
+        Log.d("IK BENEEN NIEUWE THREAD", "Ik Ga nu de database vullen met start data");
 
 
-                    // Insert bewoners
+        // Insert bewoners
 
-                    getBewonerDAO().insert(new Bewoner("Steven", "00-00-0000", true, true));
-                    getBewonerDAO().insert(new Bewoner("Thijs", "21-12-1995", false, true));
-                    getBewonerDAO().insert(new Bewoner("Sven", "22-02-1996", false, true));
-                    getBewonerDAO().insert(new Bewoner("vG Tinder", "10-4-1998", false, true));
-                    getBewonerDAO().insert(new Bewoner("vG KVN", "20-11-1997", false, true));
+        getBewonerDAO().insert(new Bewoner("Steven", "00-00-0000", true, true));
+        getBewonerDAO().insert(new Bewoner("Thijs", "21-12-1995", false, true));
+        getBewonerDAO().insert(new Bewoner("Sven", "22-02-1996", false, true));
+        getBewonerDAO().insert(new Bewoner("vG Tinder", "10-4-1998", false, true));
+        getBewonerDAO().insert(new Bewoner("vG KVN", "20-11-1997", false, true));
 
-                    Log.d("Bewoners", "geinstert");
+        Log.d("Bewoners", "geinstert");
 
-                    List<Bewoner> list = getBewonerDAO().getAllBewoners();
-                    Log.d("we hebben: ", "" + list.size() + " bewoners");
+        List<Bewoner> list = getBewonerDAO().getAllBewoners();
+        Log.d("we hebben: ", "" + list.size() + " bewoners");
 
-                    // Insert schoonmaakbier relaties
+        // Insert schoonmaakbier relaties
 
-                    Log.d("SchoonmaakBier", "Inserting all relaties");
+        Log.d("SchoonmaakBier", "Inserting all relaties");
 
 
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Thijs", "Sven", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Thijs", "vG Tinder", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Thijs", "vG KVN", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Thijs", "Steven", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Thijs", "Sven", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Thijs", "vG Tinder", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Thijs", "vG KVN", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Thijs", "Steven", 0));
 
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Sven", "Thijs", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Sven", "vG Tinder", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Sven", "vG KVN", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Sven", "Steven", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Sven", "Thijs", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Sven", "vG Tinder", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Sven", "vG KVN", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Sven", "Steven", 0));
 
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG Tinder", "Thijs", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG Tinder", "Sven", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG Tinder", "vG KVN", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG Tinder", "Steven", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG Tinder", "Thijs", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG Tinder", "Sven", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG Tinder", "vG KVN", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG Tinder", "Steven", 0));
 
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG KVN", "Thijs", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG KVN", "Sven", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG KVN", "vG Tinder", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG KVN", "Steven", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG KVN", "Thijs", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG KVN", "Sven", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG KVN", "vG Tinder", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "vG KVN", "Steven", 0));
 
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Steven", "Thijs", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Steven", "Sven", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Steven", "vG Tinder", 0));
-                    getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Steven", "vG KVN", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Steven", "Thijs", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Steven", "Sven", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Steven", "vG Tinder", 0));
+        getSchoonmaakBierDAO().insert(new SchoonmaakBier(0, "Steven", "vG KVN", 0));
 
-                    Log.d("Schoonmaakbier", "All relaties inserted. ");
+        Log.d("Schoonmaakbier", "All relaties inserted. ");
 
-                    Log.d("Database", "Database populated");
+        Log.d("Database", "Database populated");
 
     }
 
@@ -112,7 +121,7 @@ public abstract class MMDatabase extends RoomDatabase {
 
         // Dirty way to check if this database already has data. I could not find a good way to do this
         List<Bewoner> listBewoners = getBewonerDAO().getAllBewoners();
-        if(!listBewoners.isEmpty()) {
+        if (!listBewoners.isEmpty()) {
             return;
         }
 
@@ -169,8 +178,8 @@ public abstract class MMDatabase extends RoomDatabase {
 
     }
 
-    protected void printDB(){
-        Log.d("=================","=============");
+    protected void printDB() {
+        Log.d("=================", "=============");
         Log.d("Printing database", "gl");
 
         List<Bewoner> bewoners = getBewonerDAO().getAllBewoners();
@@ -181,158 +190,104 @@ public abstract class MMDatabase extends RoomDatabase {
         // Total beers to be received
         Map<String, Integer> sbBeerReceives = new HashMap<>();
 
-        for(Bewoner bewoner : bewoners) {
+        for (Bewoner bewoner : bewoners) {
             Log.d("Printing sbbier for", bewoner.getNaam());
             List<SchoonmaakBier> sblist = getSchoonmaakBierDAO().getAllSchoonmaakBierBewonerReceives(bewoner.getNaam());
-            for(SchoonmaakBier sb : sblist) {
+            for (SchoonmaakBier sb : sblist) {
                 Log.d("From" + sb.getToGive(), Integer.toString(sb.getBeer()));
 
-                if(sbBeerReceives.containsKey(bewoner.getNaam())){
-                    sbBeerReceives.put(bewoner.getNaam(), sbBeerReceives.get(bewoner.getNaam())+sb.getBeer());
+                if (sbBeerReceives.containsKey(bewoner.getNaam())) {
+                    sbBeerReceives.put(bewoner.getNaam(), sbBeerReceives.get(bewoner.getNaam()) + sb.getBeer());
                 } else {
                     sbBeerReceives.put(bewoner.getNaam(), sb.getBeer());
                 }
 
-                if(sbBeerOwed.containsKey(sb.getToGive())){
-                    sbBeerOwed.put(sb.getToGive(), sbBeerOwed.get(sb.getToGive())+sb.getBeer());
+                if (sbBeerOwed.containsKey(sb.getToGive())) {
+                    sbBeerOwed.put(sb.getToGive(), sbBeerOwed.get(sb.getToGive()) + sb.getBeer());
                 } else {
-                   sbBeerOwed.put(sb.getToGive(), sb.getBeer());
+                    sbBeerOwed.put(sb.getToGive(), sb.getBeer());
                 }
 
             }
         }
 
 
-
-        for(String naam : sbBeerReceives.keySet()){
+        for (String naam : sbBeerReceives.keySet()) {
             Log.d("RECEIVE AND OWE FOR", naam);
             Log.d("receive", "ex:" + Integer.toString(sbBeerReceives.get(naam)) +
                     "ac:" + Integer.toString(getSchoonmaakBierDAO().getSchoonmaakBiertoReceiveSum(naam)));
-            Log.d("owe", "ex:"+ Integer.toString(sbBeerOwed.get(naam)) +
+            Log.d("owe", "ex:" + Integer.toString(sbBeerOwed.get(naam)) +
                     "ac:" + Integer.toString(getSchoonmaakBierDAO().getSchoonmaakBiertoGiveSum(naam)));
         }
-        Log.d("-------------","------------");
-        Log.d("=================","=============");
+        Log.d("-------------", "------------");
+        Log.d("=================", "=============");
     }
 
-    // TODO: Finish export to JSON
-    public void exportToJSON(String filename, final Context context){
-        Log.d("export to JSON", "Creating thread");
-        /*        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {*/
-        AsyncTask.execute(new Runnable(){
-            @Override
-            public void run() {
-                Log.d("Exporting to JSON", " Gathering data");
-
-                // First gather data
-                List<Bewoner> bewoners = getBewonerDAO().getAllBewoners();
-                List<SchoonmaakBier> sb = getSchoonmaakBierDAO().getAllSchoonmaakBier();
-
-                // Convert to JSON
-                Log.d("converting to JSON", " Gathering data");
-                Gson gson = new Gson();
-                Log.d("" + gson.toJson(bewoners), "");
-                Log.d("" + gson.toJson(sb), "");
-
-                String res = gson.toJson(bewoners) + gson.toJson(sb);
-                // export file
-                Log.d("Trying to try", "========= ");
-
-
-                OutputStreamWriter output = null;
-                try {
-                    Log.d("trying to write to file","");
-                    output = new OutputStreamWriter(context.openFileOutput(Calendar.getInstance().getInstance().getTime() + "", Context.MODE_PRIVATE));
-                    output.write(gson.toJson(bewoners) + gson.toJson(sb));
-                    output.close();
-                } catch (FileNotFoundException e) {
-                    Log.d("FileNotFoundException", " faf");
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    Log.d("IOEXCEPTION", "eH");
-                    e.printStackTrace();
-                }
-
-
-                Log.d("FUCK ME THEN", "========= ");
-            }
-        });
-    }
-
-
-    // TODO: should this destroy the previous database? or override it? Delete for now
-    // TODO: make sure this is safe, this function is easy to abuse
-    // TODO: possibly remove this function
-    public static void importJSON(String filename){
-
-        AsyncTask.execute(new Runnable(){
-            @Override
-            public void run(){
-
-            }
-        });
-    }
 
     // Process requested beers function
     public synchronized void processBeer(Map<String, Integer> orders) {
         Log.d("5555555555555", "55555555555");
-        Log.d("Processing beer for","multiple people");
+        Log.d("Processing beer for", "multiple people");
 
+        Map<String, Integer> orders_out = new HashMap<String, Integer>(orders);
         // For each person that wants a beer, check where to deduct or add the beer to
         // The key here is a string of the name of the person requesting, the value is the amount of beers requested
         for (String key : orders.keySet()) {
             int beers = orders.get(key);
             Log.d(beers + " beers ordered by", key);
-            if(beers == 0) {
+            if (beers == 0) {
+                orders_out.remove(key);
                 continue;
             }
 
             processBeer(key, beers);
         }
 
+        // Write orders to file
+        beerDataToFile(orders_out);
+
     }
 
-    public synchronized void processBeer(String bewoner, int beersToBeAccounted){
+    public synchronized void processBeer(String bewoner, int beersToBeAccounted) {
+        beerDataToFile(bewoner, beersToBeAccounted);
+
         int beers = beersToBeAccounted;
 
-        Log.d("0000000000000","0000000");
-        Log.d("PROCESSING " + Integer.toString(beers),"beer for" + bewoner);
+        Log.d("0000000000000", "0000000");
+        Log.d("PROCESSING " + Integer.toString(beers), "beer for" + bewoner);
 
         getBewonerDAO().addGedronkenBier(beers, bewoner);
 
 
         List<SchoonmaakBier> sbList = getSchoonmaakBierDAO().getAllSchoonmaakBierBewonerReceives(bewoner);
 
-        for(SchoonmaakBier sb : sbList) {
+        for (SchoonmaakBier sb : sbList) {
             Log.d("beers left", Integer.toString(beers));
-            if(sb.getBeer() < 0){
+            if (sb.getBeer() < 0) {
                 Log.e(bewoner + Integer.toString(sb.getBeer()) + " from", sb.getToGive());
                 continue;
             }
-            if(sb.getBeer() == 0){
+            if (sb.getBeer() == 0) {
                 Log.d(bewoner + " no sb from", sb.getToGive());
                 continue;
             }
 
             // Bewoners receives some sb from sb.getToGive()
 
-            if(beers <= sb.getBeer()) {
+            if (beers <= sb.getBeer()) {
                 // all beers can be substracted from sb beer
                 Log.d("Subbing rest " + Integer.toString(beers) + " sb from ", sb.getToGive());
                 getSchoonmaakBierDAO().setBeer(sb.getBeer() - beers, sb.getToReceive(), sb.getToGive());
-                Log.d("sb.getbeer()-beers", Integer.toString(sb.getBeer()-beers));
+                Log.d("sb.getbeer()-beers", Integer.toString(sb.getBeer() - beers));
 
                 //there are no more beers left
                 return;
-            }
-            else if(beers > sb.getBeer()){
+            } else if (beers > sb.getBeer()) {
                 //Some can be substracted here
                 Log.d("Some sb subbed", Integer.toString(sb.getBeer()) + " from " + sb.getToGive());
 
                 getSchoonmaakBierDAO().setBeer(0, sb.getToReceive(), sb.getToGive());
-                Log.d("beers-sb.getBeer", Integer.toString(beers-sb.getBeer()));
+                Log.d("beers-sb.getBeer", Integer.toString(beers - sb.getBeer()));
                 beers -= sb.getBeer();
             }
 
@@ -345,6 +300,75 @@ public abstract class MMDatabase extends RoomDatabase {
     }
 
 
+    // Write ordered beer data to file
+    protected void beerDataToFile(Map<String, Integer> orders) {
+        Log.d("Writing Data to file", "WOOT");
+        try {
+            // Create directory
+            File dir = new File(path);
+            if(dir.mkdirs()){
+                Log.d("Directory creatededs:", "YEBABE");
+            }
+
+
+            File file = new File(path + filename);
+            if (!file.exists()) {
+                if(file.createNewFile()){
+                    Log.d("FILE SUCCESFULLY CREATE", "GODDOMN");
+                }
+            }
+
+            FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+            fileOutputStream.write(("Op " + getDateAndTime() + ", werd er bier besteld door:\n").getBytes());
+
+            for (String key : orders.keySet()) {
+                int beers = orders.get(key);
+                fileOutputStream.write(("\t\t" + Integer.toString(beers) + " door: " + key + "\n").getBytes());
+            }
+
+            fileOutputStream.close();
+
+        } catch (IOException e) {
+            Log.e("IO", e.getMessage());
+        }
+
+    }
+
+    // Write ordered beer data to file
+    protected void beerDataToFile(String bewoner, int beer) {
+        Log.d("Writing Data to file", "WOOT");
+        try {
+            // Create directory
+            File dir = new File(path);
+            if(dir.mkdirs()){
+                Log.d("Directory creatededs:", "YEBABE");
+            }
+
+
+            File file = new File(path + filename);
+            if (!file.exists()) {
+                if(file.createNewFile()){
+                    Log.d("FILE SUCCESFULLY CREATE", "GODDOMN");
+                }
+            }
+
+            FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+            fileOutputStream.write(("Op " + getDateAndTime() + ", werd er bier besteld door:\n").getBytes());
+
+            fileOutputStream.write(("\t\t" + Integer.toString(beer) + " door: " + bewoner + "\n").getBytes());
+
+            fileOutputStream.close();
+
+        } catch (IOException e) {
+            Log.e("IO", e.getMessage());
+        }
+
+    }
+
+
+    public static String getDateAndTime(){
+        return Calendar.getInstance().getTime().toString();
+    }
 
 }
 
